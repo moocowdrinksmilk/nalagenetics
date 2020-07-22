72a62f33e833f4f2847b37e3246b41fbb47921b5
Rui Xian Tay <ruixiantay@Ruis-MacBook-Pro.local>
2020-07-22 17:07:45 +0800

    Updates

---
+++ precache-manifest.ea87aa43c8348ba8cbbe9024c76eeb59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f77233a560f862645e60a141c57d1f5",
+    "url": "/nalagenetics/index.html"
+  },
+  {
+    "revision": "302627cfabd1932e0584",
+    "url": "/nalagenetics/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "ba705a016facd278a91f",
+    "url": "/nalagenetics/static/css/main.943ebbc5.chunk.css"
+  },
+  {
+    "revision": "302627cfabd1932e0584",
+    "url": "/nalagenetics/static/js/2.99459219.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/nalagenetics/static/js/2.99459219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba705a016facd278a91f",
+    "url": "/nalagenetics/static/js/main.8c3fccfa.chunk.js"
+  },
+  {
+    "revision": "3ad22e3a073bc416994e",
+    "url": "/nalagenetics/static/js/runtime-main.35e67e31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nalagenetics/static/media/logo.5d5d9eef.svg"
+  }
+]);
